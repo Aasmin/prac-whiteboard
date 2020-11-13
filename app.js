247aba976b01091b05312a113fae9ea75ddb3760
Aasmin <aasmin.kainth@gmail.com>
2020-11-13 13:45:34 -0800

    more features and updates

--- app.js
+++ app.js
@@ -5,38 +5,50 @@
 // Install scripts from: https://www.christianengvall.se/electron-packager-tutorial/ 
 //To start server: node app
 
-// api  
-const express = require('express');
-const app = express();  //server ko app kehte hein
+// server side
+const express = require("express");
+// express server
+const app = express();
+//  nodejs
+const server = require("http").Server(app);
+// nodejs => socket enabled
+const path = require("path");
+const io = require("socket.io")(server);
+// serve static assets to client
+app.use(express.static("public"));
+// server
+io.on("connection", function(socket) {
+  socket.on("size", function(size) {
+    socket.broadcast.emit("onsize", size);
+  });
+  socket.on("color", function(color) {
+    socket.broadcast.emit("oncolor", color);
+  });
 
-//  nodejs module
-const httpServer = require('http').createServer(app);
-//  socket enbaled server 
-const io = require('socket.io')(httpServer);
-io.on("connection", function (socket) {
-    console.log("New client connected");
-    console.log(socket.id);
-    socket.on("color", function (color) {   //event listener
-        console.log(data);
-        socket.broadcast.emit('colorchange', color);
-    })
-    socket.on("md", function (point) {
-        socket.broadcast.emit("onmd", point);
-    })
-    socket.on("mm", function (point) {
-        socket.broadcast.emit("onmm", point);
-    })
-})
-app.use(express.static("Client"));
-// app.get("/", function (req, res) {   //127.0.0.1 = localhost
-//     res.end("<h1>Welcome to home Page</h1>")
-// })
-
-//  connection - server machine hai jo listen krti | 3000 = port 
-let port = process.env.PORT || 3000;
-httpServer.listen(port, function () {
-    console.log("Server started at port 3000");
-}) 
+  socket.on("toolchange", function(tool) {
+    socket.broadcast.emit("ontoolchange", tool);
+  });
+  socket.on("hamburger", function() {
+    socket.broadcast.emit("onhamburger");
+  });
+  socket.on("mousedown", function(point) {
+    socket.broadcast.emit("onmousedown", point);
+  });
+  socket.on("mousemove", function(point) {
+    socket.broadcast.emit("onmousemove", point);
+  });
+  socket.on("undo", function() {
+    socket.broadcast.emit("onundo");
+  });
+  socket.on("redo", function() {
+    socket.broadcast.emit("onredo");
+  });
+});
+// nodejs server
+const port = process.env.PORT || 3000;
+server.listen(port, function(req, res) {
+  console.log("Server has started at port 3000");
+});
 
 //Types of request: Get, Post, Patch(Update), Delete
 
